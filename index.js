6ef6cab72902aa5f7a06316f924b0796aeb15044
VRRemi <business@vrremi.xyz>
2022-01-18 17:13:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const config = require("./config.js");
 const client = new Client({disableMentions: "everyone", disabledEvents: ["TYPING_START"], intents: 32767, partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 const events = require("./structures/event");
 const command = require("./structures/slash");
+
