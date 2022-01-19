7ae430e396b49905c5bae9b36cb6fd03b95356e0
VRRemi <business@vrremi.xyz>
2022-01-18 18:03:23 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose");
 module.exports = async (client) => {
     client.user.setActivity(`/record`)
     require("../dashboard/index")(client);
-    await mongoose.connect(client.config.mongodb. {
+    await mongoose.connect(client.config.mongodb, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        useUnifiedTopology: true
     }, (err) => {
         if (err) return console.error(err);
         console.log(`${client.user.username} database is connected...`)
     });
-
+    
     console.log(`${client.user.username} bot is connected...`);
     client.guilds.cache.map(guild => {
         commands = guild ? guild.commands : client.application?.commands
-        client.comannds.map(cmd => commands.create(cmd.help).catch(async () => {}));
+        client.commands.map(cmd => commands.create(cmd.help).catch(async () => {}));
     });
-});
+};
