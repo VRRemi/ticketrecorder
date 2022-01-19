78b2ef7835f0b0c232c4c03a0a20799e686ee71b
VRRemi <business@vrremi.xyz>
2022-01-18 17:12:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const {Client , Collection} = require("discord.js");
 const config = require("./config.js");
+const client = new Client({disableMentions: "everyone", disabledEvents: ["TYPING_START"], intents: 32767, partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
