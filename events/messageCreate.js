eab4ed2b098d587cdc45e364b8b962407756f106
VRRemi <business@vrremi.xyz>
2022-01-20 11:10:33 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,6 +6,7 @@ let { RandomString } = require("../utils");
 module.exports = async (client, message) => {
     if (message.author.bot || message.channel.type == "DM") return;
     if (["stop recording", "stop", "end recording", "end"].includes(message.content.toLowerCase())) {
+        let check = await RECORDS.findOne({ id: message.guildId, channel: message.channelId, author: message.author.id })
         
     }
 }
