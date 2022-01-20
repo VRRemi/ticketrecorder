7707a51d55fa242832510644fe6455c0f935b0ec
VRRemi <business@vrremi.xyz>
2022-01-20 12:14:39 -0800

    Update record.js

--- slash/record.js
+++ slash/record.js
@@ -47,6 +47,7 @@ module.exports.run = async (client, interaction, options) => {
                         let user = await interaction.member.guild.members.cache.get(id);
                         if (!user) {
                             replyEmbed(`**\`${id}\` has been excluded, couldn't find them in the server**`, channel)
+                            ids.push(`${id}-declined`);
                         };
                     }
                 });
