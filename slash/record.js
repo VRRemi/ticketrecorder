351f3a7e550a91f72dd068d0eda5b2d8944ef20c
VRRemi <business@vrremi.xyz>
2022-01-20 12:09:46 -0800

    Update record.js

--- slash/record.js
+++ slash/record.js
@@ -6,4 +6,5 @@ module.exports.run = async (client, interaction, options) => {
     let check = await RECORDS.findOne({ id: interaction.guildId, channel: interaction.channelId, author: interaction.member.id });
     if (check) {
         limits.delete(interaction.channelId+interaction.member.id);
+        return interaction.reply("**You've already active recording process in this channel**");
     }
