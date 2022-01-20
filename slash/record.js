de1287953e4326621be730f85f07ea68bbed5a6a
VRRemi <business@vrremi.xyz>
2022-01-20 12:09:32 -0800

    Update record.js

--- slash/record.js
+++ slash/record.js
@@ -1,3 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 let RECORDS = require("../models/records");
 let limits = new Map();
+
+module.exports.run = async (client, interaction, options) => {
+    }
