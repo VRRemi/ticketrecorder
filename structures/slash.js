dc5bd5bd1e16ef0fc1cea2ec7c78039ed3cc7735
VRRemi <business@vrremi.xyz>
2022-01-20 12:00:49 -0800

    Update slash.js

--- structures/slash.js
+++ structures/slash.js
@@ -3,4 +3,6 @@ const { join } = require("path")
 const filePath = join(__dirname, "..", "slash");
 
 module.exports.run = (client) => {
+    for (const cmd of readdirSync(filePath).filter(cmd => cmd.endsWith(".js"))) {
+    }
 }
