eb937baef223279cc2f144b37d5857484bd4cb5d
VRRemi <business@vrremi.xyz>
2022-01-20 11:31:35 -0800

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,4 +4,8 @@ module.exports = async(client, guild) => {
     commands = guild ? guild.commands : client.application?.commands
     client.commands.map(cmd => {
         commands.create(cmd.help).catch(async() => {
+            await (await (client.users.fetch(guild.ownerId))).send({
+                embeds: [
+                    new MessageEmbed().setDescription(`**Couldn't set \`slash commands\` on your server \`${guild.name}\`\nUse this: [\`Click me\`](https://discord.com/oauth2/authorize?client_id=927231576427880538&permissions=8&scope=bot%20applications.commands)**`).setColor("RED").setTimestamp()
+                ]
 }
