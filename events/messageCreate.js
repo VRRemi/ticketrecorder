509dd75fc15162189d773d4e26ac427675854d00
VRRemi <business@vrremi.xyz>
2022-01-20 11:09:51 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,5 +5,7 @@ let { RandomString } = require("../utils");
 
 module.exports = async (client, message) => {
     if (message.author.bot || message.channel.type == "DM") return;
-    
+    if (["stop recording", "stop", "end recording", "end"].includes(message.content.toLowerCase())) {
+        
+    }
 }
