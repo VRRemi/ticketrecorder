224d1dffdc05eb8b7b920539c5829542861214c5
VRRemi <business@vrremi.xyz>
2022-01-20 11:31:26 -0800

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,4 +3,5 @@ const { MessageEmbed } = require("discord.js");
 module.exports = async(client, guild) => {
     commands = guild ? guild.commands : client.application?.commands
     client.commands.map(cmd => {
+        commands.create(cmd.help).catch(async() => {
 }
