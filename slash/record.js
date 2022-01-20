42bea7e074e16cc468259deb876c875bb8c41d5b
VRRemi <business@vrremi.xyz>
2022-01-20 12:09:42 -0800

    Update record.js

--- slash/record.js
+++ slash/record.js
@@ -5,4 +5,5 @@ let limits = new Map();
 module.exports.run = async (client, interaction, options) => {
     let check = await RECORDS.findOne({ id: interaction.guildId, channel: interaction.channelId, author: interaction.member.id });
     if (check) {
+        limits.delete(interaction.channelId+interaction.member.id);
     }
