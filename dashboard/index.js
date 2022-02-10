a634205dbc9ddee035efd746e16a9e997147d38d
VRRemi <business@vrremi.xyz>
2022-02-10 14:01:08 -0800

    Update index.js

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,3 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = (client) => {
+    const { port } = require("../config");
+    const fastify = require('fastify')({ logger: false });
+    const { Liquid } = require("liquidjs");
+    const path = require("path");
