cf2904c6f08d8b678d8ebc9652f97bbee15e8f2c
VRRemi <business@vrremi.xyz>
2022-02-10 14:16:02 -0800

    Update oauth2.js

--- dashboard/routes/route[get]/auth/oauth2.js
+++ dashboard/routes/route[get]/auth/oauth2.js
@@ -3,3 +3,16 @@ const config = require("../../../../config");
 
 const discordoauth2 = (fastify, options, done) => {
 
+    fastify.register(oauth2, {
+        name: 'discordOAuth2',
+        credentials: {
+            client: {
+                id: config.clientID,
+                secret: config.clientSecret
+            },
+            auth: oauth2.DISCORD_CONFIGURATION
+        },
+        scope: ['identify'],
+        startRedirectPath: '/auth',
+        callbackUri: `${config.domain}/callback`
+    })
