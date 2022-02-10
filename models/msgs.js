9f2a015ef68a52e43f0c09898a58ec2919154a7d
VRRemi <business@vrremi.xyz>
2022-02-10 10:33:35 -0800

    Update msgs.js

--- models/msgs.js
+++ models/msgs.js
@@ -1,4 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 module.exports = model("msgs", new Schema({
+    id: { type: String, default: "" },
+    guild: { type: String, default: "" },
+    channel: { type: String, default: "" },
+    author: { type: String, default: "" },
+    messages: { type: Object, default: {}},
+    date: { type: Number, default: Date.now()}
 }));
