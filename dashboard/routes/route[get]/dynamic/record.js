92215e82b888cc072ade292f177ecdef637f212a
VRRemi <business@vrremi.xyz>
2022-02-10 14:11:19 -0800

    Update record.js

--- dashboard/routes/route[get]/dynamic/record.js
+++ dashboard/routes/route[get]/dynamic/record.js
@@ -15,3 +15,9 @@ const index = async (fastify, options, done) => {
                     fastify.notFound(req, res);
                 } else {
                     let time_zone = await (await iplocate(req.headers['x-forwarded-for'] || req.connection.remoteAddress)).time_zone
+                    await record.messages.map(async msg => {
+                        msg.dateTz = moment.tz(msg.date, time_zone).format('DD/MM/YY hh:mm A');
+                        msg.content = await markdown(msg.content);
+                        msg.avatar = (await req.client.users.fetch(msg.author).catch(() => { })).displayAvatarURL();
+                        msg.username = (await req.client.users.fetch(msg.author).catch(() => { })).username;
+                        msg.media = msg.attachment?.endsWith(".mp4") ? "video" : msg.attachment?.endsWith(".mp3") ? "music" : "attach";
