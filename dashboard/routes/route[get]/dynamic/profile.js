901647f2bb4454da78bca236b2ab7e36e9bdb0f2
VRRemi <business@vrremi.xyz>
2022-02-10 14:06:49 -0800

    Update profile.js

--- dashboard/routes/route[get]/dynamic/profile.js
+++ dashboard/routes/route[get]/dynamic/profile.js
@@ -1,4 +1,11 @@
 let MSGS = require("../../../../models/msgs");
 
 const index = async(fastify, options, done) => {
+    fastify.get("/profile", async(req, res) => {
+        if (!(await req.user)) res.redirect("/auth");
+        else {
+            let ids = []
+            await (await MSGS.find({})).filter(message => {
+                message.messages.map(async msg => {
+                    if (msg.author == (await req.user).id) return !ids.includes(message.id) ? ids.push(message.id) : ""
 };
