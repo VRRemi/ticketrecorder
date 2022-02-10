b42c266acd55c5e064a76c3803ef5f62976b74c5
VRRemi <business@vrremi.xyz>
2022-02-10 14:06:52 -0800

    Update profile.js

--- dashboard/routes/route[get]/dynamic/profile.js
+++ dashboard/routes/route[get]/dynamic/profile.js
@@ -8,4 +8,8 @@ const index = async(fastify, options, done) => {
             await (await MSGS.find({})).filter(message => {
                 message.messages.map(async msg => {
                     if (msg.author == (await req.user).id) return !ids.includes(message.id) ? ids.push(message.id) : ""
+                })
+            });
+            req.render("/dynamic/profile.liquid", { ids, avatar: (await req.client.users.fetch((await req.user).id).catch(() => {})).displayAvatarURL() })
+        }
 };
