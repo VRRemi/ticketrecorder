1bbd4252f46345746dbb124b792adefa7654bbcd
VRRemi <business@vrremi.xyz>
2022-02-10 10:33:24 -0800

    Update records.js

--- models/records.js
+++ models/records.js
@@ -1,4 +1,9 @@
 const { Schema, model } = require("mongoose");
 
 module.exports = model("records", new Schema({
+    id: { type: String, default: "" },
+    channel: { type: String, default: "" },
+    author: { type: String, default: "" },
+    records: { type: Array, default: [] },
+    messages: { type: Array, default: [] }
 }));
