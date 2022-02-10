121a3b1daf3f104abdfb28be80faf0a484436e39
VRRemi <business@vrremi.xyz>
2022-02-10 14:11:15 -0800

    Update record.js

--- dashboard/routes/route[get]/dynamic/record.js
+++ dashboard/routes/route[get]/dynamic/record.js
@@ -4,3 +4,13 @@ let { markdown } = ("../../../../utils");
 const iplocate = require("node-iplocate");
 
 const index = async (fastify, options, done) => {
+    fastify.get("/transcript/:record", async (req, res) => {
+        if (!req.params.record) fastify.notFound(req, res)
+        else {
+            let record = await MSGS.findOne({ id: req.params.record })
+            if (!record) fastify.notFound(req, res);
+            else {
+                if (record.messages.length <= 0) {
+                    record.deleteOne();
+                    fastify.notFound(req, res);
+                } else {
