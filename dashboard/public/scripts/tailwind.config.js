86ec0ce22b340db7bd48c82b2aaa03f608e3f8ee
VRRemi <business@vrremi.xyz>
2022-02-25 11:17:07 -0800

    Update tailwind.config.js

--- dashboard/public/scripts/tailwind.config.js
+++ dashboard/public/scripts/tailwind.config.js
@@ -29,3 +29,6 @@ tailwind.config = {
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
